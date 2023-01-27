d763e475e8c3818550721abd6b793df84478b1b9
maendeh <120230275+maendeh@users.noreply.github.com>
2023-01-26 22:19:07 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,13 @@
-#terraform block
 terraform {
-  required_version = "~> 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0" # Optional but recommended in production
+      version = "~> 4.0"
     }
   }
 }
 
-
-#provider block
+# Configure the AWS Provider
 provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+  region = "us-east-1"
 }
